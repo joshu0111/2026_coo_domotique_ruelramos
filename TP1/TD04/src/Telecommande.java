c88400fd79d8a3293f0f4df5e06d747e218b9bd9
Joshua Ruel Ramos <joshua.ruel-ramos3@etu.univ-lorraine.fr>
2026-04-03 09:40:58 +0200

    Modification de la javadoc de la classe Telecommande.java

--- TP1/TD04/src/Telecommande.java
+++ TP1/TD04/src/Telecommande.java
@@ -1,52 +1,55 @@
-/*
-* La classe telecommande est une classe qui permet de stocker une liste de telemcommande
-*/
+/**
+ * La classe telecommande est une classe qui permet de stocker une liste de telemcommande
+ */
 
 public class Telecommande {
 
-    /*
-        Constructeur de la classe
+    /**
+     * Constructeur de la classe
      */
 //    public Telecommande(){
 //
 //    }
 
 
-    /*
-        Methode ajouterLampe qui permet d'ajouter la lampe qui est fourni en parametre aux telecommandes
-        @param l est un objet Lampe
+    /**
+     * Methode ajouterLampe qui permet d'ajouter la lampe qui est fourni en parametre aux telecommandes
+     * @param l est un objet Lampe
      */
     public void ajouterLampe(Lampe l){
         throw new Error (" code non ecrit ");
     }
 
 
-    /*
-        Methode qui active une lampe a parir de son indice
-        @param indiceLampe entier qui correspond a l'indice de la lampe
+    /**
+     * Methode qui active une lampe a parir de son indice
+     * @param indiceLampe entier qui correspond a l'indice de la lampe
      */
     public void activerLampe(int indiceLampe){
         throw new Error (" code non ecrit ");
     }
 
 
-    /*
-        Methode qui desactive une lampe a parir de son indice
-        @param indiceLampe entier qui correspond a l'indice de la lampe
-    */
+    /**
+     * Methode qui desactive une lampe a parir de son indice
+     * @param indiceLampe entier qui correspond a l'indice de la lampe
+     */
     public void desactiverLampe(int indiceLampe){
         throw new Error (" code non ecrit ");
     }
 
 
-    /*
-        Methode act lampe qui permet d'ajouter une lampe a partir de son indice dans la liste des telecommades
-    */
+    /**
+     * Methode act lampe qui permet d'ajouter une lampe a partir de son indice dans la liste des telecommades
+     */
     public void activerTout(){
         throw new Error (" code non ecrit ");
     }
 
-
+    /**
+     * Methode toString qui renvoie l'affichage de la classe
+     * @return String
+     */
     @Override
     public String toString() {
         return "Telecommande{}";
