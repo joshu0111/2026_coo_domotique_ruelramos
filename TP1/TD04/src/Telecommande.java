b899d78cb0176282fbf9d7ab8cb6cee901fe194c
Joshua Ruel Ramos <joshua.ruel-ramos3@etu.univ-lorraine.fr>
2026-04-03 09:29:07 +0200

    Modification de la classe Telecommande

--- TP1/TD04/src/Telecommande.java
+++ TP1/TD04/src/Telecommande.java
@@ -1,4 +1,54 @@
-package PACKAGE_NAME;
+/*
+* La classe telecommande est une classe qui permet de stocker une liste de telemcommande
+*/
 
 public class Telecommande {
+
+    /*
+        Constructeur de la classe
+     */
+//    public Telecommande(){
+//
+//    }
+
+
+    /*
+        Methode ajouterLampe qui permet d'ajouter la lampe qui est fourni en parametre aux telecommandes
+        @param l est un objet Lampe
+     */
+    public void ajouterLampe(Lampe l){
+        throw new Error (" code non ecrit ");
+    }
+
+
+    /*
+        Methode qui active une lampe a parir de son indice
+        @param indiceLampe entier qui correspond a l'indice de la lampe
+     */
+    public void activerLampe(int indiceLampe){
+        throw new Error (" code non ecrit ");
+    }
+
+
+    /*
+        Methode qui desactive une lampe a parir de son indice
+        @param indiceLampe entier qui correspond a l'indice de la lampe
+    */
+    public void desactiverLampe(int indiceLampe){
+        throw new Error (" code non ecrit ");
+    }
+
+
+    /*
+        Methode act lampe qui permet d'ajouter une lampe a partir de son indice dans la liste des telecommades
+    */
+    public void activerTout(){
+        throw new Error (" code non ecrit ");
+    }
+
+
+    @Override
+    public String toString() {
+        return "Telecommande{}";
+    }
 }
