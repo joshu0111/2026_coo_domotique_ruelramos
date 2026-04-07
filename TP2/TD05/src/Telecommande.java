f3af3655fc5540aa22d8692d2a5100d642214151
Joshua Ruel Ramos <joshua.ruel-ramos3@etu.univ-lorraine.fr>
2026-04-07 10:50:08 +0200

    Modification classe Telecommande pour adapter a la classe peripherique

--- TP2/TD05/src/Telecommande.java
+++ TP2/TD05/src/Telecommande.java
@@ -8,89 +8,56 @@ import java.util.Iterator;
 public class Telecommande {
 
     /** Attributs */
-    ArrayList<Lampe> lampes;
-    ArrayList<Hifi> hifis;
+    ArrayList<Peripherique> peripheriques;
 
     /**
      * Constructeur de la classe
      */
     public Telecommande(){
-        lampes = new ArrayList<>();
+        peripheriques = new ArrayList<>();
     }
 
 
     /**
-     * Methode ajouterLampe qui permet d'ajouter la lampe qui est fourni en parametre aux telecommandes
-     * @param l est un objet Lampe
+     * Methode ajouterPeripherique qui permet d'ajouter un peripherique qui est fourni en parametre aux telecommandes
+     * @param p est un objet qui implement l'interface Peripherique
      */
-    public void ajouterLampe(Lampe l){
-        lampes.add(l);
+    public void ajouterPeripherique(Peripherique p){
+        peripheriques.add(p);
     }
 
 
     /**
-     * Methode qui active une lampe a parir de son indice
-     * @param indiceLampe entier qui correspond a l'indice de la lampe
+     * Methode qui active un peripherique a parir de son indice
+     * @param indicePeripherique entier qui correspond a l'indice du peripherique
      */
-    public void activerLampe(int indiceLampe){
-        lampes.get(indiceLampe).allumer();
+    public void activerPeripheriques(int indicePeripherique){
+        peripheriques.get(indicePeripherique).allumer();
     }
 
 
     /**
-     * Methode qui desactive une lampe a parir de son indice
-     * @param indiceLampe entier qui correspond a l'indice de la lampe
+     * Methode qui desactive un peripherique a parir de son indice
+     * @param indicePeripherique entier qui correspond a l'indice du peripherique
      */
-    public void desactiverLampe(int indiceLampe){
-        lampes.get(indiceLampe).eteindre();
+    public void desactiverPeripherique(int indicePeripherique){
+        peripheriques.get(indicePeripherique).eteindre();
     }
 
 
     /**
-     * Methode act lampe qui permet d'ajouter une lampe a partir de son indice dans la liste des telecommades
+     * Methode activerTout qui permet d'ajouter un peripherique a partir de son indice dans la liste des telecommades
      */
-    public void activerToutLampe(){
-        Iterator<Lampe> i = lampes.iterator();
+    public void activerTout(){
+        Iterator<Peripherique> i = pheripheriques.iterator();
         while (i.hasNext()) i.next().allumer();
     }
 
 
-    /**
-     * Methode qui permet d'activer une chaine hi-fi a partir de son indice dans la liste des chaines hi-fi
-     * @param indiceHifi
-     */
-    public void activerHifi(int indiceHifi){
-        hifis.get(indiceHifi).allumer();
-    }
-
-    /**
-     * Methode qui permet de desactiver une chaine hi-fi
-     * @param indiceHifi
-     */
-    public void desactiverHifi(int indiceHifi){
-        hifis.get(indiceHifi).eteindre();
-    }
-
-
-    /**
-     * Methode qui permet d'activer toutes les chaines hi-fi
-     */
-    public void activerToutHifi(){
-        Iterator<Hifi> i = hifis.iterator();
-        while (i.hasNext()) i.next().allumer();
-    }
-
-
-    /**
-     * Methode toString qui renvoie l'affichage de la classe
-     *
-     * @return String
-     */
-    @Override
-    public String toString() {
+    @java.lang.Override
+    public java.lang.String toString() {
         return "Telecommande{" +
-                "hifis=" + hifis +
-                ", lampes=" + lampes +
+                "peripheriques=" + peripheriques +
                 '}';
     }
 }
