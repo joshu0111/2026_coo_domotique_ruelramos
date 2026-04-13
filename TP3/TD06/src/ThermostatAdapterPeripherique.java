5604db4549a97d39bc0e78c961fe746c3d4d2bcd
Joshua Ruel Ramos <joshua.ruel-ramos3@etu.univ-lorraine.fr>
2026-04-13 23:17:25 +0200

    Creation du fichier ThermostatAdapterPeripherique.java et modifcation de diagramme uml

---
+++ TP3/TD06/src/ThermostatAdapterPeripherique.java
@@ -0,0 +1,18 @@
+import thermos.Thermostat;
+
+/**
+ * Classe qui permet d'implement les classes de l'interface Peripherique pour pourvoir etre utiliser parr une telecommande
+ */
+public class ThermostatAdapterPeripherique {
+    /**
+     * Attributs
+     */
+    Thermostat th;
+
+    /**
+     * Constructeur de la classe
+     */
+    public ThermostatAdapterPeripherique(){
+        
+    }
+}
